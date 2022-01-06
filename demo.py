8376b096a65d03b5c170d6e11abf5bc1627a83ea
danalves24com <danalves24@outlook.com>
2022-01-06 22:59:23 +0100

    first version of freebody

---
+++ demo.py
@@ -0,0 +1,19 @@
+import freebody
+import math
+
+testObj = {
+    "forces": [
+        {
+            "name": "Friction",
+            "magnitude": 20,
+            "theta": math.pi / 2
+        },
+        {
+            "name": "Pull",
+            "magnitude": 10,
+            "theta": math.pi/4
+        }
+    ]
+}
+
+freebody.freebody(testObj)
