40ae8a1a5a7a6e3a158ea9a2f772456ff0094bd6
danalves24com <danalves24@outlook.com>
2022-01-07 16:03:21 +0100

    docs

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ A free-body diagram generator for Python 🔲
 # Installation
 ```pip3 install pyfreebody```
 # Usage
-The following system setup produces [this output](./examples/simple.out.png)
+The following system setup produces [this output](./examples/simple.out.png). Please view documentation [here](./docs.org)
 ![simple demo](./examples/simple.png)
 
--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyfreebody',
-    version='0.1.0',
+    version='0.1.2',
     author="Daniel Rosel",
     url="https://github.com/danalves24com/pyfreebody",
-    long_description = "README.md",
     packages=find_packages(include=['pyfreebody']),
         install_requires=[
             "Pillow"
