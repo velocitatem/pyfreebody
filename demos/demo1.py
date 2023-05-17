306ea2b147fcf36a48ab9fd76da65237fd91fbc0
Daniel Rosel <daniel@alves.world>
2023-05-17 15:39:40 +0200

    Initial changes and fixes

--- demos/demo1.py
+++ demos/demo1.py
@@ -1,6 +1,7 @@
-from pyfreebody import Freebody, Direction
+from pyfreebody.pyfreebody import Freebody, Direction, SystemType
+import math
 
-sysinc = math.pi/6
+sysinc = math.pi/4
 body = Freebody("inclinedplane", 20, SystemType.inclinedPlane, sysinc)
 body.addForce("Normal", 200, sysinc + math.pi/2)
 body.diagram()
