9d5d998825746ab5a690d2df90842b5e140ffeb0
Peter Kaplan <peter.kaplan@gmail.com>
2023-08-06 12:01:43 -0400

    documentation

--- demos/demo1.py
+++ demos/demo1.py
@@ -1,7 +1,8 @@
-from pyfreebody.pyfreebody import Freebody, Direction, SystemType
+from pyfreebody.pyfreebody import Freebody, Direction, SystemType, blueColor, LegendType
 import math
 
 sysinc = math.pi/4
-body = Freebody("inclinedplane", 20, SystemType.inclinedPlane, sysinc)
+body = Freebody("inclinedplane", 20, SystemType.inclinedPlane, sysinc,
+color=blueColor, legend=LegendType.arrow)
 body.addForce("Normal", 200, sysinc + math.pi/2)
 body.diagram()
