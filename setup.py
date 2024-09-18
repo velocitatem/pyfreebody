0e5bbe7654d205436ede76ae059bd7807baf1e1c
Daniel Rosel <daniel@alves.world>
2024-09-18 12:02:14 +0200

    funding an new V

--- setup.py
+++ setup.py
@@ -4,8 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyfreebody',
-    version='0.4.1',
+    version='0.4.2',
     author="Daniel Rosel",
+    author_email='daniel@alves.world',
+    description='A package for generating freebody diagrams.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url="https://github.com/velocitatem/pyfreebody",
     packages=find_packages(),
         install_requires=[
