f532adf888e9e522ff59270839e419f2948b827f
Daniel Rosel <daniel@alves.world>
2024-09-18 11:14:45 +0200

    changed version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyfreebody',
-    version='0.3.2',
+    version='0.4.0',
     author="Daniel Rosel",
     url="https://github.com/velocitatem/pyfreebody",
     packages=find_packages(include=['pyfreebody']),
